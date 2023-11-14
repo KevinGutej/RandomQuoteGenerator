d2b5b794be8dd3aa1ee0deb013d4d8796512647b
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-11-14 22:43:31 +0100

    Initial Commit

--- index.php
+++ index.php
@@ -1,74 +1,32 @@
-<?php
-// Array of quotes
-$quotes = [
-    "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do.",
-    "Whatever you can do, or dream you can, begin it. Boldness has genius, power, and magic in it.",
-    "The purpose of our lives is to be happy.",
-    "Life is really simple, but we insist on making it complicated.",
-    "Life is what happens when you're busy making other plans.",
-    "The best time to plant a tree was 20 years ago. The second best time is now.",
-    "The purpose of our lives is to be happy.",
-    "Life is what happens when you're busy making other plans.",
-    "Get busy living or get busy dying.",
-    "You have within you right now, everything you need to deal with whatever the world can throw at you.",
-    "The only way to do great work is to love what you do.",
-    "Life is really simple, but we insist on making it complicated.",
-    "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do.",
-    "In three words I can sum up everything I've learned about life: it goes on.",
-    "The best time to plant a tree was 20 years ago. The second best time is now.",
-    "The purpose of our lives is to be happy.",
-    "Life is what happens when you're busy making other plans.",
-    "Get busy living or get busy dying.",
-    "You have within you right now, everything you need to deal with whatever the world can throw at you.",
-    "The only way to do great work is to love what you do.",
-    "Life is really simple, but we insist on making it complicated.",
-    "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do.",
-    "In three words I can sum up everything I've learned about life: it goes on.",
-    "The best time to plant a tree was 20 years ago. The second best time is now.",
-    "The purpose of our lives is to be happy.",
-    "Life is what happens when you're busy making other plans.",
-    "Get busy living or get busy dying.",
-    "You have within you right now, everything you need to deal with whatever the world can throw at you.",
-    "The only way to do great work is to love what you do.",
-    "Life is really simple, but we insist on making it complicated.",
-    "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do.",
-    "In three words I can sum up everything I've learned about life: it goes on.",
-    "The best time to plant a tree was 20 years ago. The second best time is now.",
-    "The purpose of our lives is to be happy.",
-];
-
-
-$randomQuote = $quotes[array_rand($quotes)];
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Random quote generator</title>
+    <title>Document</title>
     <style>
         body {
             text-align: center;
             background-color: black;
-            padding: 50px;
+            margin: 0;
+            padding: 0;
         }
-
-        .quote-box {
-            max-width: 600px;
-            margin: 0 auto;
-            background-color: white;
-            border-radius: 10px;
-            padding: 20px;
+        .galleryContainer {
+            max-width: 800px;
+            margin: 50px auto;
+        }
+        .slideshowContainer {
+            position: relative;
+            max-width: 100%;
+            margin: auto;
         }
     </style>
 </head>
-
 <body>
-    <div class="quote-box">
-        <p><?php echo $randomQuote; ?></p>
-    </div>
-</body>
+<div class="galleryContainer">
+        <div class="slideshowContainer">
 
+        </div>
+</div>
+</body>
 </html>
